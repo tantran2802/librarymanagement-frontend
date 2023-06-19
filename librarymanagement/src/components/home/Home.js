b1ff349b914899f6fec339bef168c77e373950fe
Tan Ngoc Tan <tantran2802@gmail.com>
2023-06-19 08:14:48 +0700

    update get all authors

--- librarymanagement/src/components/home/Home.js
+++ librarymanagement/src/components/home/Home.js
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react";
 function Home() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        function getDataFromFetchingHttp() {
+            fetch('http://localhost:8080/auth/authors', {
+                method: 'get'
+            })
+                .then(async res => await res.json())
+                .then(async data => setData(data))
+                .catch(err => console.log(err))
+        }
+        getDataFromFetchingHttp();
+    }, [])
 
     return (
         <div>
-            This is Home!
+            {data ? data.map((item,index) => {
+                return (
+                    <div key={index + 1}>
+                        <p style={{color:"grey"}}>{index + 1} - {item.firstName} {item.lastName}</p>
+       
+                    </div>
+                )
+            }) : 'Loading...'}
         </div>
 
     );
 }
 export default Home;
+
+
