8faa7d5ca83e7792dc9727cdde97126b438c98f9
Tan Ngoc Tran <tantran2802@gmail.com>
2023-06-22 14:33:44 +0700

    update feature dashboard

--- librarymanagement/src/components/navigation/Layout.js
+++ librarymanagement/src/components/navigation/Layout.js
@@ -1,8 +1,11 @@
 import Navi from "./navigation";
-import classes from './layout.module.css'
+import classes from './layout.module.css';
+import SideBar from '../Sidebar/index';
+import sidebar_menu from '../../constants/sidebar-menu'
 export default function Layout(props){
     return(
-        <div>
+        <div className={classes.structure}>
+            <SideBar menu={sidebar_menu} />
             <Navi />
             <main className={classes.main}>{props.children}</main>
         </div>
